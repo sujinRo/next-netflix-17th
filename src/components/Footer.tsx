64ae2ce60c527eebc6fb3575902a69ace5c7539c
yoojin <tlsdbwls17@ewhain.net>
2023-05-13 06:08:09 +0900

    feat: Home페이지 합침

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,35 +7,31 @@ import { BsList } from 'react-icons/bs';
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
   position: fixed;
   bottom: 0;
-  justify-content: center;
   width: 375px;
   height: 48px;
   background: #121212;
-`;
-const Contents = styled.div`
-  display: flex;
-  width: 320px;
-  height: 45px;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
 `;
 
 const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  cursor: pointer;
+  /*
   :hover {
     color: white;
-    cursor: pointer;
   }
+  */
 `;
 
 const Text = styled.div`
   font-size: 8.2px;
-  weight: 500;
+  font-weight: 500;
   margin-top: 3px;
   color: #8c8787;
 `;
@@ -43,28 +39,26 @@ const Text = styled.div`
 export default function Footer() {
   return (
     <Wrapper>
-      <Contents>
-        <Content>
-          <BiHomeAlt2 size={'22px'} color="#8C8787" />
-          <Text>Home</Text>
-        </Content>
-        <Content>
-          <FiSearch size={'22px'} color="#8C8787" />
-          <Text>Search</Text>
-        </Content>
-        <Content>
-          <MdOutlineVideoLibrary size={'22px'} color="#8C8787" />
-          <Text>Coming Soon</Text>
-        </Content>
-        <Content>
-          <HiDownload size={'22px'} color="#8C8787" />
-          <Text>Downloads</Text>
-        </Content>
-        <Content>
-          <BsList size={'22px'} color="#8C8787" />
-          <Text>More</Text>
-        </Content>
-      </Contents>
+      <Content>
+        <BiHomeAlt2 size={'22px'} color="#8C8787" />
+        <Text>Home</Text>
+      </Content>
+      <Content>
+        <FiSearch size={'22px'} color="#8C8787" />
+        <Text>Search</Text>
+      </Content>
+      <Content>
+        <MdOutlineVideoLibrary size={'22px'} color="#8C8787" />
+        <Text>Coming Soon</Text>
+      </Content>
+      <Content>
+        <HiDownload size={'22px'} color="#8C8787" />
+        <Text>Downloads</Text>
+      </Content>
+      <Content>
+        <BsList size={'22px'} color="#8C8787" />
+        <Text>More</Text>
+      </Content>
     </Wrapper>
   );
 }
